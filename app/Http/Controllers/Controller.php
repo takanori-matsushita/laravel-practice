c010f65ac857e0baee4f4e7a8053085e680d3904
takanori-matsushita <t.matsushita@arcjobz.com>
2020-04-08 13:51:41 +0900

    Add hello

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,10 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+  use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+  public function hello()
+  {
+    return '<html><body><h1>Hello World</h1></body></html>';
+  }
 }
