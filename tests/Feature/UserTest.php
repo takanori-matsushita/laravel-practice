cafa33edbdad9a408b9002f2fd6be9ce9fa0891d
takanori-matsushita <t.matsushita@arcjobz.com>
2020-04-17 03:00:50 +0900

    Make a basic User model (including secure passwords)

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Tests\Feature;
+
+use \App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\UserFormRequest;
+
+class UserTest extends TestCase
+{
+  /**
+   * A basic feature test example.
+   * 
+   * @dataProvider userDataProvider
+   * @return void
+   */
+
+  public function testShouldBeValid($name, $email, $expect)
+  {
+    $request = new UserFormRequest();
+    $rules = $request->rules();
+    $item = ["name" => $name, "email" => $email, "password" => "password"];
+    $validator = Validator::make($item, $rules);
+    $result = $validator->passes();
+    $this->assertEquals($expect, $result);
+  }
+
+  /**
+   * A basic feature test example.
+   * 
+   * @dataProvider emailDataProvider
+   * @return void
+   */
+
+  public function testEmailUpper($email, $expect)
+  {
+    $emailUpper = strtoupper($email);
+    $result = $this->stringUpper($emailUpper);
+    $this->assertEquals($expect, $result);
+  }
+
+  private function stringUpper($emailUpper)
+  {
+    $users = User::all();
+    foreach ($users as $user) {
+      if ($emailUpper === strtoupper($user["email"])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * A basic feature test example.
+   * 
+   * @dataProvider emailDataProvider
+   * @return void
+   */
+
+  public function testEmaillower($email, $expect)
+  {
+    $emailLower = strtolower($email);
+    $result = $this->stringLower($emailLower);
+    $this->assertEquals($expect, $result);
+  }
+
+  private function stringLower($emailUpper)
+  {
+    $users = User::all();
+    foreach ($users as $user) {
+      if ($emailUpper === strtolower($user["email"])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * A basic feature test example.
+   * 
+   * @dataProvider passwordDataProvider
+   * @return void
+   */
+
+  public function testPasswordInvalid($password, $expect)
+  {
+    $request = new UserFormRequest();
+    $rules = $request->rules();
+    $item = ["name" => "test", "email" => "example@laravel.com", "password" => $password];
+    $validator = Validator::make($item, $rules);
+    $result = $validator->passes();
+    $this->assertEquals($expect, $result);
+  }
+
+  public function userDataProvider()
+  {
+    $this->createApplication();  //Userモデルを使用する際に
+    $user = User::find(1)->toArray();
+    return [
+      'true' => ["Example User", "user@example.com", true],
+      //nameバリデーション
+      'name required error' => ["", "user@example.com", false],
+      'name max_length error' => [str_repeat("a", 51), "user@example.com", false],
+      //emailバリデーション
+      'email required error' => ["Example User", "", false],
+      'email max_length error' => ["Example User", str_repeat("a", 244) . "@example.com", false],
+      'email unique error' => [$user["name"], $user["email"], false],
+      //emailが有効なパターン
+      'email user@example.com' => ["Example User", "user@example.com", true],
+      'email USER@foo.COM' => ["Example User", "USER@foo.com", true],
+      'email A_US-ER@foo.bar.org' => ["Example User", "A_US-ER@foo.bar.org", true],
+      'email first.last@foo.jp' => ["Example User", "first.last@foo.jp", true],
+      'email alice+bob@baz.cn' => ["Example User", "alice+bob@baz.cn", true],
+      //emailが無効なパターン
+      'email  user@example,com' => ["Example User", "user@example,com", false],
+      'email  user_at_foo.org' => ["Example User", "user_at_foo.org", false],
+      'email  user.name@example.' => ["Example User", "user.name@example.", false],
+      'email  foo@bar_baz.com' => ["Example User", "foo@bar_baz.com", false],
+      'email  foo@bar+baz.com' => ["Example User", "foo@bar+baz.com", false],
+      'email  foo@bar..com' => ["Example User", "foo@bar..com", false],
+    ];
+  }
+
+  public function emailDataProvider()
+  {
+    $user = User::find(1)->toArray();
+    return [
+      'email upper lower passed' => ["user@example.com", false],
+      'email upper lower error' => [$user["email"], true],
+    ];
+  }
+
+  public function passwordDataProvider()
+  {
+    return [
+      'blank password' => [str_repeat(" ", 6), false],
+      'minimum length password' => [str_repeat("a", 5), false],
+    ];
+  }
+}
